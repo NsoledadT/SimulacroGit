aede554f68318418dcfc77eb414d920088ace34d
mscalzotto <maxiscalzotto@gmail.com>
2014-04-29 19:35:41 -0300

    cambios en index.html y validacion del nombre

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 function validar(){
+	//Marcos
+	var nombre = document.getElementById('nom').value; 
+ 	var expRegNom = /[a-zA-Z]/;
+
+if(nombre == ""){
+	alert("El campo nombre no puede estar vacio");
+	return false;
+}else if(!(expRegNom.test(nombre))){
+ 	alert("El nombre solo puede contener letras");
+ 	return false;
+ }
+
 //Natalia
 var sexo = document.getElementsByName("Sexo");
 if(sexo[0].checked == false && sexo[1].checked == false)
@@ -6,7 +18,8 @@ if(sexo[0].checked == false && sexo[1].checked == false)
   alert("No se ha seleccionado ningun sexo");
    return false;
  }
- else{
-   return true;
-  }
-};
+ 
+ return true;
+ 
+
+}
